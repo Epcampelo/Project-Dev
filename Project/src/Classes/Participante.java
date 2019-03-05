3e9238db81ebd16a9ab16bbe27f326e6e18c93c9
Emanuel <emanuelpereira96@yahoo.com>
2019-03-05 17:02:25 -0300

    ---

---
+++ Project/src/Classes/Participante.java
@@ -0,0 +1,15 @@
+
+package Classes;
+
+public class Participante {
+    private String nome;
+    private String acento; // algum tipo de mapeamento de acentos??
+    
+    public void comprar_ingresso (){
+        escolher_acento();
+        
+    }
+    public void escolher_acento () {
+        
+    }
+}
